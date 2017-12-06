45c5cc6adb2d4d45f267f4957d5d9f322ab46a83
Mathijs Smit <msmit@smit-it.info>
2017-12-06 11:50:28 +0100

    Amazon AWS config fixes

--- src/Login/init_container.sh
+++ src/Login/init_container.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 service ssh start
 
-[ -z "$ASPNETCORE_URLS" ] && export ASPNETCORE_URLS=http://*:"$PORT"
+[ -z "$ASPNETCORE_URLS" ] && export ASPNETCORE_URLS=http://*:8000
 
 # If there is any command line argument specified, run it
 [ $# -ne 0 ] && exec "$@"
